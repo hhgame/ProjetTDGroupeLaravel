9f281e9099b20c76adc04dc10383fe274994b65e
Pas1713 <pascaline.pichon.17@gmail.com>
2025-04-29 11:27:32 +0200

    Revert "Merge branch 'main' of https://github.com/hhgame/ProjetTDGroupeLaravel"
    This reverts commit 5525c35ab1de6d7f688a6430472645eaa8898744, reversing
    changes made to 9eea971c49e6820a00cfebd2f2aa4d07dfeb461f.

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,9 +52,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Category::class);
     }
-
-    public function contacts()
-    {
-        return $this->hasMany(Contact::class);
-    }
 }
